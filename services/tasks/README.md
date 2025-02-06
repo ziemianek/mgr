88816b72400bebdc3ed23eab818105a81f958ad4
Michał Ziemianek <46460816+ziemianek@users.noreply.github.com>
2025-02-06 19:58:42 +0100

    Repo cleanup (#33)
    * CLEANUP HUUUUUUGE
    
    * prepared repo template for futher development

---
+++ services/tasks/README.md
@@ -0,0 +1,4 @@
+Run tasks service locally
+```sh
+uvicorn services.tasks.app.main:app --host 0.0.0.0 --port 5050 --reload
+```
---
+++ services/tasks/app/example_data.py
@@ -0,0 +1,22 @@
+from typing import Any, Final
+
+TASKS: Final[list[dict[str, Any]]] = [
+    {
+        "task_id": 1,
+        "task_name": "Task 1",
+        "task_description": "Testowy task",
+        "completed": False
+    },
+    {
+        "task_id": 2,
+        "task_name": "Task 2",
+        "task_description": "This is Task 2",
+        "completed": False
+    },
+    {
+        "task_id": 3,
+        "task_name": "Task 3",
+        "task_description": "This is Task 3",
+        "completed": True
+    },
+]
